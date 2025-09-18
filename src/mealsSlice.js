5166a95252bae10cf49ee7b09e06776bce63d44a
Aditya Panicker <adipanicker7@gmail.com>
2025-09-18 22:54:08 +0530

    update

--- src/mealsSlice.js
+++ src/mealsSlice.js
@@ -5,7 +5,7 @@ export const mealsSlice = createSlice({
   name: "meals",
   initialState: [
     { name: "Breakfast", cost: 50, selected: false },
-    { name: "High Tea", cost: 25, selected: false },
+    { name: "High Tea", cost: 15, selected: false },
     { name: "Lunch", cost: 65, selected: false },
     { name: "Dinner", cost: 75, selected: false },
   ],
